79d598e4525e1fb7306dc07def328a802c51e5cc
Matheus Gaspar <matheusgs2509@gmail.com>
2022-10-02 19:50:00 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/pages/section1/App';
 import Beneficio from './components/pages/section2/Beneficio';
+import Chat from './components/pages/section3/Chat';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
     <Beneficio />
+    <Chat />
+
   </React.StrictMode>
 );
 
