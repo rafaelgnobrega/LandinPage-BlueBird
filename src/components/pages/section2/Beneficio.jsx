fd1a45d8234dbdf3f7af58002733c73e3826ba5c
FehS1lva <felipppe.eg@hotmail.com>
2022-10-02 22:06:14 -0300

    iniciando secao 2

--- src/components/pages/section2/Beneficio.jsx
+++ src/components/pages/section2/Beneficio.jsx
@@ -1,10 +1,29 @@
-import React from   'react';
+import React from 'react';
 import './Beneficio.css';
+import Mao from '../../image/mao.svg';
 
 function Beneficio() {
-    return (
-      <div className="container2">Teste!</div>
-    );
-  }
-  
-  export default Beneficio;
+  return (
+    <div className="container2">
+
+      <section>
+        <h2>
+          Por que contratar o seguro de automóvel da <span>BlueBird</span>?
+        </h2>
+        <div className="seguraBenecifio">
+          <div className="Beneficio1">
+            <img src={Mao} />
+            <div className="texto">
+            <p className="titulo">Contratação facilitada</p>
+            <p>O processo é simples e, com as coberturas adicionai, você deixa tudo do seu jeito</p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+    </div>
+
+  );
+}
+
+export default Beneficio;
