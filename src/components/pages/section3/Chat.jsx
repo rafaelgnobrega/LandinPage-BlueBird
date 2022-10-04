2a74058d2c50612d9c5ea9e30ec2c28f5286fac5
Matheus Gaspar <matheusgs2509@gmail.com>
2022-10-03 23:16:12 -0300

    testeee

--- src/components/pages/section3/Chat.jsx
+++ src/components/pages/section3/Chat.jsx
@@ -7,8 +7,8 @@ function Chat() {
     <div className="container3">
       <section>
           <div className="container_chat">
-              <h1>Converse com um de nossos corretores pelo chat</h1> 
-              <p>Um seguro com preço competitivo, contratação facilitada e agilidade no atendimento</p>
+              <h1>Converse com um de <br></br> nossos corretores pelo chat</h1> 
+              <p>Um seguro com preço competitivo, contratação facilitada e agilidade <br></br> no atendimento</p>
               <button><a href="#">Fazer cotação</a></button>
               </div>
           </section>
