6b7f900e51a04e0e77d9593131d94d6a83c321df
Rafael Nóbrega <90426277+rafaelgnobrega@users.noreply.github.com>
2022-10-03 22:42:31 -0300

    Atualizando Section 3

--- src/components/pages/section3/Chat.jsx
+++ src/components/pages/section3/Chat.jsx
@@ -3,9 +3,18 @@ import './Chat.css';
 
 
 function Chat() {
-    return (
-      <div className="container3">Teste!</div>
-    );
-  }
-  
-  export default Chat;
+  return (
+    <div className="container3">
+      <section>
+          <div className="container_chat">
+              <h1>Converse com um de nossos corretores pelo chat</h1> 
+              <p>Um seguro com preço competitivo, contratação facilitada e agilidade no atendimento</p>
+              <button><a href="#">Fazer cotação</a></button>
+              </div>
+          </section>
+          </div>
+
+  );
+}
+
+export default Chat;
