71887d9f1e77dbb09ae0988167e615e4edbeb12a
Rafael Nóbrega <90426277+rafaelgnobrega@users.noreply.github.com>
2022-10-11 17:10:39 -0300

    Finalizando responsividade das Sections 3 e 4

--- src/components/pages/section4/Seguros.jsx
+++ src/components/pages/section4/Seguros.jsx
@@ -8,21 +8,25 @@ function Seguros() {
                 <h2>Coberturas</h2>
                 <p className="coberturas">Com a <span>BlueBird</span> Seguros, você conta com as principais coberturas:</p>
                 <div className="container-card">
-                    <div className="card1">
-                        <p className="title">Incêndio</p>
-                        <p className="tipo-seguro">Veículos em <br />chamas</p>
+                    <div className='segura-dupla'>
+                        <div className="card1">
+                            <p className="title">Incêndio</p>
+                            <p className="tipo-seguro">Veículos em <br />chamas</p>
+                        </div>
+                        <div className="card2">
+                            <p className="title">Colisão</p>
+                            <p className="tipo-seguro">Colisão de <br />veículos</p>
+                        </div>
                     </div>
-                    <div className="card2">
-                        <p className="title">Colisão</p>
-                        <p className="tipo-seguro">Colisão de <br />veículos</p>
-                    </div>
-                    <div className="card3">
-                        <p className="title">Guincho</p>
-                        <p className="tipo-seguro">Assistência com<br />Reboque</p>
-                    </div>
-                    <div className="card4">
-                        <p className="title">Furto</p>
-                        <p className="tipo-seguro">Seguro para<br />veículos roubados</p>
+                    <div className='segura-dupla2'>
+                        <div className="card3">
+                            <p className="title">Guincho</p>
+                            <p className="tipo-seguro">Assistência com<br />Reboque</p>
+                        </div>
+                        <div className="card4">
+                            <p className="title">Furto</p>
+                            <p className="tipo-seguro">Seguro para<br />veículos roubados</p>
+                        </div>
                     </div>
                 </div>
                 <h3>Entre <span className='negrito'>vááários</span> outros tipos</h3>
