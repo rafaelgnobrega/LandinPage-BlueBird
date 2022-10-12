d2dde722d58ba64e023a91f291d4f5a375f47c1f
Rafael Nóbrega <90426277+rafaelgnobrega@users.noreply.github.com>
2022-10-12 01:31:39 -0300

    Finalizando Responsivo do site

--- src/components/pages/section6/cotacao.jsx
+++ src/components/pages/section6/cotacao.jsx
@@ -13,20 +13,29 @@ function Cotacao() {
                 <form action="https://formsubmit.co/bluebird.suporte@gmail.com" method="POST">
                     <div className="container-input">
                         <div className="esquerda">
-                            <p>Nome completo</p>
-                            <input type="text" id="name" name="Nome" placeholder="    Nome completo" required></input>
-                            <p>Digite seu e-mail</p>
-                            <input type="email" id="email" name="Email" placeholder="    Email" required></input>
+                            <div className="nameInputs">
+                                <p>Nome completo</p>
+                                <input type="text" id="name" name="Nome" placeholder="    Nome completo" required></input>
+                            </div>
+                            <div className="nameInputs">
+                                <p>Digite seu e-mail</p>
+                                <input type="email" id="email" name="Email" placeholder="    Email" required></input>
+                            </div>
                         </div>
                         <div className="direita" >
-                            <p>Data de nascimento</p>
-                            <input type="date" id="date" name="Data de Nascimento" placeholder="    Data de nascimento" required></input>
-                            <p>Telefone</p>
-                            <input type="text" id="telefone" name="Telefone" placeholder="    Digite seu telefone" required></input>
+                            <div className="nameInputs">
+                                <p>Data de nascimento</p>
+                                <input type="date" id="date" name="Data de Nascimento" placeholder="    Data de nascimento" required></input>
+                            </div>
+                            <div className="nameInputs">
+                                <p>Telefone</p>
+                                <input type="text" id="telefone" name="Telefone" placeholder="    Digite seu telefone" required></input>
+                            </div>
                         </div>
                     </div>
                     <button type='submit'>Solicitar cotação</button>
                     <input type="hidden" name="_subject" value="Solicitação de Cotação" />
+                    <input type="hidden" name="_captcha" value="false"/>
                     <input type="hidden" name="_autoresponse" value="Nós da BlueBird recebemos a sua solicitação para realizar a cotação de seguros de seu veículo. Entraremos em contato com você dentro de 24 horas!"/>
                 </form>
             </section>
